86a9cec35d3319dfcac233eeea0029efe3c51190
GabrielaNQ <U23234041@utp.edu.pe>
2026-05-24 13:50:18 -0500

    avance inicio y conexion de header con inicio y nosotros

--- src/frontend/src/app/pages/header/header.component.ts
+++ src/frontend/src/app/pages/header/header.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-type PageView = 'catalog' | 'detail' | 'auth';
+type PageView = 'inicio' | 'nosotros' | 'catalog' | 'detail' | 'auth';
 
 @Component({
   selector: 'app-header',
@@ -12,9 +12,11 @@ type PageView = 'catalog' | 'detail' | 'auth';
 })
 export class HeaderComponent {
 
-  @Input() activePage: PageView = 'catalog';
+  @Input() activePage: PageView = 'inicio';
 
   @Output() catalogClicked = new EventEmitter<void>();
+  @Output() inicioClicked = new EventEmitter<void>();
+  @Output() nosotrosClicked = new EventEmitter<void>();
   @Output() loginClicked = new EventEmitter<void>();
   @Output() registerClicked = new EventEmitter<void>();
 
