74892f005b457a6da4efbf8f3a04559d60e0dab6
GabrielaNQ <U23234041@utp.edu.pe>
2026-05-25 14:35:09 -0500

    Termino de diseño de seccion inicio

--- src/frontend/src/app/pages/inicio/inicio.ts
+++ src/frontend/src/app/pages/inicio/inicio.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
- import { BuscadorInteligente } from '../shared/components/buscador-inteligente/buscador-inteligente';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { BuscadorInteligente } from '../shared/components/buscador-inteligente/buscador-inteligente';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
 
   imports: [
+    CommonModule,
     BuscadorInteligente
   ],
 
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css'
+  styleUrls: ['./inicio.css']
 })
 
 export class Inicio {
+
+  @Output() registerClicked = new EventEmitter<void>();
+
+  @Output() catalogClicked = new EventEmitter<void>();
 
 }
